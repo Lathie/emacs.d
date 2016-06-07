7ebd8da8aaec5e8b6f9f696746e2fc436d27dfca
Victor <vniu@justworks.com>
2016-06-07 17:16:50 -0400

    Current iteration of stuff

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+'(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -26,19 +26,50 @@
   (package-initialize))
 
 ;;(load-theme 'blackboard t)
-(require 'solarized)
+;(require 'solarized)
 
-(deftheme solarized-dark "The dark variant of the Solarized colour theme")
+;(deftheme solarized-dark "The dark variant of the Solarized colour theme")
 
-(create-solarized-theme 'dark 'solarized-dark)
+;(create-solarized-theme 'dark 'solarized-dark)
 
-(provide-theme 'solarized-dark)
+;(provide-theme 'solarized-dark)
 
 ;;(global-linum-mode 1)
 
-(set-default-font "Anonymous Pro-18") 
+;(set-default-font "Anonymous Pro-18") 
 
 (setq-default indent-tabs-mode t)
 (setq tab-width 1) ; or any other preferred value
     (defvaralias 'c-basic-offset 'tab-width)
     (defvaralias 'cperl-indent-level 'tab-width)
+
+;helm stufffff
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
+
+(global-set-key (kbd "C-x g") 'magit-status)
