445d388dd28e21b1e949e4acf96578e46e162328
Lathie <niuvictor@gmail.com>
2016-08-25 18:08:01 -0500

    updates

--- elpa/fireplace-20160101.1047/fireplace-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; fireplace-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (fireplace) "fireplace" "fireplace.el" (22278 64622
-;;;;;;  41242 515000))
-;;; Generated autoloads from fireplace.el
-
-(autoload 'fireplace "fireplace" "\
-Light the fire.
-
-\(fn ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("fireplace-pkg.el") (22278 64622 222589
-;;;;;;  134000))
-
-;;;***
-
-(provide 'fireplace-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; fireplace-autoloads.el ends here
--- elpa/fireplace-20160101.1047/fireplace-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fireplace" "20160101.1047" "A cozy fireplace for emacs" (quote nil))
