5e3b0ebebe7ca832a6d5ba230f90b8f0c27226ee
Lathie <niuvictor@gmail.com>
2020-03-07 00:40:53 -0800

    CLEAN UP

--- init.el
+++ init.el
@@ -1,30 +1,5 @@
-(delete-selection-mode)
-(fset 'init-file
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([24 6 67108896 1 backspace 126 47 46 101 109 97 99 115 46 100 47 105 110 105 116 46 101 108 return] 0 "%d")) arg)))
-
 (blink-cursor-mode 0)
-(set-face-attribute 'default nil :height 140)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;; '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
- '(custom-safe-themes
-   (quote
-    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/org-files/permissions.org")))
- '(package-selected-packages
-   (quote
-    (avy web-mode go-mode color-theme-sanityinc-tomorrow solarized-theme magit helm-projectile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
@@ -33,55 +8,38 @@
    t)
   (package-initialize))
 
-;(load-theme 'blackboard t)
-;(require 'solarized)
-;(deftheme solarized-dark "The dark variant of the Solarized colour theme")
-;(create-solarized-theme 'dark 'solarized-dark)
-;(provide-theme 'solarized-dark)
-;(global-linum-mode 1)
-;(set-default-font "Anonymous Pro-18")
-
-(setq-default indent-tabs-mode t)
-
-;helm stufffff
+;; Set up Helm
 (require 'helm)
 (require 'helm-config)
-
-;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(helm-autoresize-mode 1)
+(helm-mode 1)
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-set-key (kbd "C-x C-f") 'helm-projectile)
 (global-unset-key (kbd "C-x c"))
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-map (kbd "C-z")  'helm-select-action)
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
-
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+(setq helm-split-window-in-side-p           t
+      helm-move-to-line-cycle-in-source     t
+      helm-ff-search-library-in-sexp        t
+      helm-scroll-amount                    8
       helm-ff-file-name-history-use-recentf t)
 
 (global-set-key (kbd "M-x") 'helm-M-x)
-(helm-autoresize-mode 1)
-
-(helm-mode 1)
-
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x f") 'helm-find-files)
 
+;; Language Spacing and such
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq-default indent-tabs-mode t)
 (setq-default require-final-newline t)
 (setq-default tab-width 2)
 (setq-default c-basic-offset 2)
 (setq-default ruby-indent-level 2)
 (setq-default js-indent-level 2)
 (setq-default indent-tabs-mode nil)
+(setq web-mode-code-indent-offset 2)
 
 (setq initial-scratch-message "")
 (setq inhibit-startup-message t)
@@ -92,6 +50,7 @@
 
 (setq tramp-default-method "ssh")
 
+(setq custom-safe-themes t)
 (require 'color-theme-sanityinc-tomorrow)
 (color-theme-sanityinc-tomorrow-eighties)
 
@@ -107,10 +66,8 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
-(setq org-agenda-files (list "~/org-files/work.org"
-														 "~/org-files/permissions.org"))
-
-(setq web-mode-code-indent-offset 2)
+(setq org-agenda-files (list "~/org/test.org"
+														 "~/org/life.org"))
 
 (global-set-key (kbd "C-c g") 'avy-goto-char)
 
@@ -119,7 +76,6 @@
             (add-hook 'before-save-hook 'gofmt-before-save)
             (setq tab-width 4)
             (setq indent-tabs-mode 1)))
-
 
 (set-frame-parameter (selected-frame) 'alpha '(95 . 85))
 (add-to-list 'default-frame-alist '(alpha . (95 . 85)))
